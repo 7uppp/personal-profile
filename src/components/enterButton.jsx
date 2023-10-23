90f2fe3ea7c0f7c28e4ac08c26e3aedf20c573d4
7uppp <markwang9315@gmail.com>
2023-10-23 10:52:22 +1000

    add just enterButton css

--- src/components/enterButton.jsx
+++ src/components/enterButton.jsx
@@ -1,11 +1,7 @@
 import '../css/enterButton.scss'
 
 const EnterButton = ({ onClick }) => {
-  return (
-    <button className="enter-button-wrapper  delayed-animation">
-      Jump in !
-    </button>
-  )
+  return <button className="enter-button-wrapper">Jump in !</button>
 }
 
 export default EnterButton
