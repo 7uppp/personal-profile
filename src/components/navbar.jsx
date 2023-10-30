78fad52a9989f35b8351292686ba3beb2da4bd1f
7uppp <markwang9315@gmail.com>
2023-10-30 15:34:52 +1000

    WIP add responsive css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,27 +1,34 @@
 import '../css/navbar.scss'
-import { useNavigate, Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import linkedInIcon from '../assets/image/linkedin.svg'
 import githubIcon from '../assets/image/github.svg'
+import toolbarIcon from '../assets/image/toolbar.svg'
 
 const Navbar = () => {
   const SKILLS = '{ SKILLS }'
-  const Navigate = useNavigate()
 
-  const handleSkillsClick = () => {
-    Navigate('/skills')
+  const closeMobileMenu = () => {
+    setToolbarClicked(false)
   }
+
+  const [toolbarClicked, setToolbarClicked] = useState(false)
   return (
     <div className="navbar-wrapper">
       <ul className="navbar-items">
         <li>
           <Link to="/">&lt;HI&gt;</Link>
         </li>
-        <li onClick={handleSkillsClick}>{SKILLS}</li>
         <li>
-          <Link to='/projects'>/&nbsp;PROJECT/</Link>
+          <Link to="/skills" onClick={closeMobileMenu}>
+            {SKILLS}
+          </Link>
         </li>
         <li>
-          <Link to='/contact'>[ CONTACT ]</Link>
+          <Link to="/projects">/&nbsp;PROJECT/</Link>
+        </li>
+        <li>
+          <Link to="/contact">[ CONTACT ]</Link>
         </li>
         <li>
           <img
@@ -40,6 +47,61 @@ const Navbar = () => {
           />
         </li>
       </ul>
+
+      <div className="navbar-mobile">
+        <img
+          className={'toolbar'}
+          src={toolbarIcon}
+          alt="toolbarIcon"
+          onClick={() => setToolbarClicked(!toolbarClicked)}
+        />
+        {toolbarClicked ? (
+          <ul className="navbar-items-mobile">
+            <li>
+              <Link to="/" onClick={closeMobileMenu}>
+                &lt;HI&gt;
+              </Link>
+            </li>
+            <li>
+              <Link to="/skills" onClick={closeMobileMenu}>
+                {SKILLS}
+              </Link>
+            </li>
+            <li>
+              <Link to="/projects" onClick={closeMobileMenu}>
+                /&nbsp;PROJECT/
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={closeMobileMenu}>
+                [ CONTACT ]
+              </Link>
+            </li>
+            <li>
+              <img
+                src={githubIcon}
+                alt="githubIcon"
+                onClick={() => {
+                  window.open('https://github.com/7uppp')
+                  closeMobileMenu()
+                }}
+              />
+            </li>
+            <li>
+              <img
+                src={linkedInIcon}
+                alt="linkedInIcon"
+                onClick={() => {
+                  window.open('https://www.linkedin.com/in/mark-wang7/')
+                  closeMobileMenu()
+                }}
+              />
+            </li>
+          </ul>
+        ) : (
+          ''
+        )}
+      </div>
     </div>
   )
 }
--- src/components/skillCard.jsx
+++ src/components/skillCard.jsx
@@ -3,8 +3,10 @@ import '../css/skillCard.scss'
 const SkillCard = ({ skillName, Icon }) => {
   return (
     <div className="skill-card">
-      <img src={Icon} alt="skillIcon" />
-      <span>{skillName}</span>
+      <div className="skill-card-wrapper">
+        <img src={Icon} alt="skillIcon" />
+        <span>{skillName}</span>
+      </div>
     </div>
   )
 }
